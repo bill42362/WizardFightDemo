6bec565b49e071c782146a3858a35db579ad0eef
bill42362 <bill42362@gmail.com>
2015-09-22 12:31:40 +0800

    Let color of ThunderNova change with time.

--- Assets/BattleController.js
+++ Assets/BattleController.js
@@ -1,16 +1,24 @@
 ﻿#pragma strict
 var wizardGameObject: GameObject;
 var thunderNovaGameObject: GameObject;
+private var epochStart = new System.DateTime(1970, 1, 1, 0, 0, 0, System.DateTimeKind.Utc);
 private var groundPlane = Plane(Vector3(0.0, 1.0, 0.0), Vector3(0, 0, 0));
-private var wizard: Wizard;
+private var lastSkillTime: double;
+private var nextSkillTime: double;
+private var wizard: Wizard; // Wizard.js
 private var wizardTargetPosition: Vector3;
+private var thunderNova: ThunderNova; // ThunderNova.js
 
 function Start () {
+	lastSkillTime = (System.DateTime.UtcNow - epochStart).TotalSeconds;
 	wizard = wizardGameObject.GetComponent(Wizard);
 	wizardTargetPosition = wizardGameObject.transform.position;
+	thunderNova = thunderNovaGameObject.GetComponent(ThunderNova);
+	nextSkillTime = lastSkillTime + thunderNova.skillTime;
 }
 
 function Update () {
+	var timestamp = (System.DateTime.UtcNow - epochStart).TotalSeconds;
 	if(Input.GetMouseButtonUp(0)) {
 		var ray: Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 		var rayDistance: float;
@@ -26,4 +34,9 @@ function Update () {
 	var thunderNovaPosition = wizardGameObject.transform.position;
 	thunderNovaPosition.y = 0;
 	thunderNovaGameObject.transform.position = thunderNovaPosition;
+	if(nextSkillTime < timestamp) {
+		lastSkillTime = lastSkillTime + thunderNova.skillTime;
+		nextSkillTime = lastSkillTime + thunderNova.skillTime;
+	}
+	thunderNova.UpdateUI(timestamp - lastSkillTime);
 }
---
+++ Assets/ThunderNova.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+var skillTime: double;
+var castingTime: double = 2.0;
+var alertTime: double = 0.8;
+var effectTime: double = 0.2;
+private var rendererObject: Renderer;
+
+function Start () {
+	skillTime = castingTime + alertTime + effectTime;
+	rendererObject = this.GetComponent.<Renderer>();
+}
+
+function Update() { }
+function UpdateUI(time: double) {
+	if(castingTime > time) {
+		rendererObject.material.color = Color(0.2, 0.6, 0.8, 0.5);
+	} else if(castingTime + alertTime > time) {
+		rendererObject.material.color = Color(0.8, 0.2, 0.6, 0.5);
+	} else {
+		rendererObject.material.color = Color(0.8, 0.6, 0.2, 0.5);
+	}
+}
